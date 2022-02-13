c63ca11abda3be1a0d796d147fcb0cfff3733b19
fernandouski <fernandolopezchs11@gmail.com>
2022-02-13 07:54:11 -0800

    Adding diagram representing Player domain model.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import {playerLost, playerRegistered, playerWon, playedTied} from "./events";
 import {leaderboardByMostWon} from "./projections";
 
 (async () => {
+        const id = '3de4644f-8282-458b-8ad5-c7b38865be44'
         const registerPlayer = await playerRegistered('fernando', 'lopes', 'flopez')
-        await playerWon('3de4644f-8282-458b-8ad5-c7b38865be44');
-        await playerLost('3de4644f-8282-458b-8ad5-c7b38865be44');
-        await playedTied('3de4644f-8282-458b-8ad5-c7b38865be44');
+        await playerWon(id);
+        await playerLost(id);
+        await playedTied(id);
         const leaderboardWin = await leaderboardByMostWon();
         console.log(leaderboardWin);
     }
